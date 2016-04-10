a337a253da6498861af93f337653e189167bac53
Ideal-Poster <malcolmgourdine@gmail.com>
2016-04-10 18:22:46 -0400

    working form created on main page.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,6 +5,16 @@ class ProjectsController < ApplicationController
   # GET /projects.json
   def index
     @projects = Project.all
+
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      flash.now[:error] = nil
+      flash.now[:notice] = 'Thank you for your message!'
+    else
+      flash.now[:error] = 'Cannot send message.'
+
+    end
   end
 
   # GET /projects/1
